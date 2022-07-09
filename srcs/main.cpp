87de1e77a3635332727be7200d64d547d3ae1fc8
Stanley SERBIN <stanleyserbin@gmail.com>
2022-07-09 18:43:41 +0200

    feat: add enable_id #43

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -90,6 +90,8 @@ int     main(int argc, char **argv)
 			test_make_pair(is_real_container);
 		if (strcmp(argv[2], "2") == 0)
 			test_is_integral(is_real_container);
+		if (strcmp(argv[2], "3") == 0)
+			test_enable_if(is_real_container);
 	} else if (strcmp(argv[1], "stack") == 0){
 		if (strcmp(argv[2], "0") == 0)
 			compile_stack(is_real_container);
