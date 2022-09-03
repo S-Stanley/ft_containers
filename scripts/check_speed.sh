db09a909da79b47663e8af3e5862fb5893cdcd4a
Stanley SERBIN <sserbin@e1r11p15.clusters.42paris.fr>
2022-09-03 14:07:39 +0200

    fix: last check

--- scripts/check_speed.sh
+++ scripts/check_speed.sh
@@ -8,21 +8,24 @@
 
 TIMEFORMAT='FT containers took %R seconds.'
 time {
-    for ((i=0; i < 21; i++))
+    for ((i=0; i < 25; i++))
     do
-        ./ft_containers map $i ft > /dev/null
+        if [ $i -ne 22 ] && [ $i -ne 18 ]
+        then
+            ./ft_containers map $i ft > /dev/null
+        fi
     done
-    for ((i=0; i < 5; i++))
+    for ((i=0; i < 3; i++))
     do
         ./ft_containers others $i ft > /dev/null
     done
-    for ((i=0; i < 6; i++))
+    for ((i=0; i < 7; i++))
     do
-        ./ft_containers others $i ft > /dev/null
+        ./ft_containers vector $i ft > /dev/null
     done
-    for ((i=0; i < 6; i++))
+    for ((i=0; i < 7; i++))
     do
-        ./ft_containers others $i ft > /dev/null
+        ./ft_containers stack $i ft > /dev/null
     done
 }
 
@@ -30,18 +33,21 @@ TIMEFORMAT='STD containers took %R took seconds.'
 time {
     for ((i=0; i < 21; i++))
     do
-        ./ft_containers map $i std > /dev/null
+        if [ $i -ne 22 ] && [ $i -ne 18 ]
+        then
+            ./ft_containers map $i std > /dev/null
+        fi
     done
-    for ((i=0; i < 5; i++))
+    for ((i=0; i < 3; i++))
     do
         ./ft_containers others $i std > /dev/null
     done
-    for ((i=0; i < 6; i++))
+    for ((i=0; i < 7; i++))
     do
-        ./ft_containers others $i std > /dev/null
+        ./ft_containers vector $i std > /dev/null
     done
-    for ((i=0; i < 6; i++))
+    for ((i=0; i < 7; i++))
     do
-        ./ft_containers others $i std > /dev/null
+        ./ft_containers stack $i std > /dev/null
     done
 }
